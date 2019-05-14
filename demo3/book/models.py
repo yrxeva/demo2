a6c8decdd2879e611e4a046927e4b12b12bfa36b
eva <eva@love.com>
2019-05-14 20:28:20 +0800

    dem2

--- demo3/book/models.py
+++ demo3/book/models.py
@@ -6,7 +6,11 @@ class Book(models.Model):
     title = models.CharField(max_length=30)
     pub_date = models.DateField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
 class Hero(models.Model):
     name = models.CharField(max_length=30)
     skill = models.CharField(max_length=100)
     bookid = models.ForeignKey(Book,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
