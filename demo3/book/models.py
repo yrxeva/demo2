f39b995770052f92d957a7abb8ca8c146e562785
eva <eva@love.com>
2019-05-14 19:42:12 +0800

    dem2

---
+++ demo3/book/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    # 创建标题属性
+    title = models.CharField(max_length=30)
+    pub_date = models.DateField(auto_now_add=True)
+
+class Hero(models.Model):
+    name = models.CharField(max_length=30)
+    skill = models.CharField(max_length=100)
+    bookid = models.ForeignKey(Book,on_delete=models.CASCADE)
