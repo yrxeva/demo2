7bf52ffa29006f7f0ede1732c17ac7459321497c
eva <eva@love.com>
2019-05-15 15:12:02 +0800

    dem2

--- demo3/book/views.py
+++ demo3/book/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse('首页')
+def detail(request,id):
+    return HttpResponse('detail %s' %id)
