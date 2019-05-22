5466eeca9d6a4e435ddfc419f9365b6a1d6456a8
eva <eva@love.com>
2019-05-22 20:25:33 +0800

    markdown使用

--- blogs/blog/views.py
+++ blogs/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,get_object_or_404
 
 from django.core.paginator import Paginator
 from .models import Article
+import markdown
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
@@ -19,5 +20,21 @@ def index(request):
 
 def detail(request,id):
     article = get_object_or_404(Article,pk=id)
+    # 使用markdown处理文本，将markdown语法转为html标签
+    # # 方法1.使用针对需要处理的article.body将markdown转为html
+    # article.body = markdown.markdown(article.body,extendions = [
+    #     'markdown.extensions.extra',
+    #     'markdown.extensions.codehilite',
+    #     'markdown.extensions.toc',
+    # ])
+
+    # 方法2.如果在外部使用目录  需要使用构造函数的写法
+    mk = markdown.Markdown(extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        'markdown.extensions.toc',
+    ])
+    article.body = mk.convert(article.body)
+    article.toc = mk.toc
     return render(request,'single.html',locals())
 
